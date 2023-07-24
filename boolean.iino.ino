5e63e2a042a5a5397cb81886a7f92dde1be1c2fb
RiyaDhami13 <132061713+RiyaDhami13@users.noreply.github.com>
2023-07-24 16:04:58 +0545

    Add files via upload

---
+++ boolean.iino.ino
@@ -0,0 +1,13 @@
+      bool myBoolean = 0;  /*or LOW; or false; */
+void setup(){
+  //put your setup code here to run once:
+  Serial.begin(9600);
+  Serial.println(myBoolean);
+  Serial.println();
+  Serial.println();
+}
+
+void loop(){
+  //put your main code here to run repeatdly:
+  
+}
