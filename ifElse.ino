290ce50de989f9bb4214498433eb5a65eb82f87a
RiyaDhami13 <132061713+RiyaDhami13@users.noreply.github.com>
2023-07-25 21:00:20 +0545

    Create ifElse.ino
    if else statement example if condition is true then the given result comes otherwise the result is "This is the else statement"

---
+++ ifElse.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(115200);
+   if (false){
+    Serial.println("This is the first statement");
+   }
+   else {
+    Serial.println("This is the else statement");
+   }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
