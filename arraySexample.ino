5d75dbdd06141f6eaa6f736fc9ec230665f362e1
RiyaDhami13 <132061713+RiyaDhami13@users.noreply.github.com>
2023-07-27 23:01:25 +0545

    Create arraySexample.ino
    It is simple example use of arrays

---
+++ arraySexample.ino
@@ -0,0 +1,15 @@
+ 
+ void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+   int myArray[8] ={ 1, 2, 3, 4, 5, 6, 7, 8};
+
+  Serial.println(myArray[5]);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
